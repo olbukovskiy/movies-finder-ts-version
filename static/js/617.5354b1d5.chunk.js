76a739b5830007a33927bed5a17d1a80e5ad0579
Oleg Bukovskiy <olbukovskiy@gmail.com>
2023-02-08 15:57:49 +0200

    Updates

---
+++ static/js/617.5354b1d5.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkmovies_finder_ts_version=self.webpackChunkmovies_finder_ts_version||[]).push([[617],{8617:function(e,n,t){t.d(n,{jTe:function(){return r}});var i=t(9983);function r(e){return(0,i.w_)({tag:"svg",attr:{viewBox:"0 0 20 20",fill:"currentColor"},child:[{tag:"path",attr:{fillRule:"evenodd",d:"M9.707 16.707a1 1 0 01-1.414 0l-6-6a1 1 0 010-1.414l6-6a1 1 0 011.414 1.414L5.414 9H17a1 1 0 110 2H5.414l4.293 4.293a1 1 0 010 1.414z",clipRule:"evenodd"}}]})(e)}}}]);
+//# sourceMappingURL=617.5354b1d5.chunk.js.map
