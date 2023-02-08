0a16f97fa83154bee4ea83e8db05fcf91a6ce3c9
Oleg Bukovskiy <olbukovskiy@gmail.com>
2023-02-08 16:11:07 +0200

    ts version

--- src/components/Cast/Cast.tsx
+++ src/components/Cast/Cast.tsx
@@ -9,20 +9,26 @@ const MoviesAPI = new SearchMoviesAPI();
 
 export const Cast = () => {
   const [cast, setCast] = useState<ICastItem[]>([]);
+  const [isLoading, setIsLoading] = useState<boolean>(true);
   const { movieId } = useParams<{ movieId: string }>();
 
   useEffect(() => {
     MoviesAPI.searchMovieCredits(movieId).then(resolve => {
       setCast(resolve!.data.cast);
+      setIsLoading(false);
     });
   }, [movieId]);
 
   return (
     <Fragment>
-      {cast.length === 0 && (
-        <div>We don't have any information about this movie crew</div>
+      {!!cast && !isLoading && (
+        <div>
+          {cast.length === 0 && (
+            <div>We don't have any information about this movie crew</div>
+          )}
+          {cast.length > 0 && <CastList cast={cast} />}
+        </div>
       )}
-      {cast.length > 0 && <CastList cast={cast} />}
     </Fragment>
   );
 };
--- src/components/Reviews/Reviews.tsx
+++ src/components/Reviews/Reviews.tsx
@@ -9,22 +9,28 @@ const MoviesAPI = new SearchMoviesAPI();
 
 export const Reviews: React.FunctionComponent = () => {
   const [reviews, setReviews] = useState<IReview[]>([]);
+  const [isLoading, setIsLoading] = useState<boolean>(true);
   const { movieId } = useParams<{ movieId: string }>();
 
   useEffect(() => {
-    MoviesAPI.searchMovieReviews(movieId).then(resolve =>
-      setReviews(resolve!.data.results)
-    );
+    MoviesAPI.searchMovieReviews(movieId).then(resolve => {
+      setReviews(resolve!.data.results);
+      setIsLoading(false);
+    });
   }, [movieId]);
 
   return (
     <Fragment>
-      {reviews.length === 0 && (
-        <div>We don't have any reviews for this movie</div>
-      )}
-      {reviews.length > 0 && (
+      {!!reviews && !isLoading && (
         <div>
-          <ReviewsList reviews={reviews} />
+          {reviews.length === 0 && (
+            <div>We don't have any reviews for this movie</div>
+          )}
+          {reviews.length > 0 && (
+            <div>
+              <ReviewsList reviews={reviews} />
+            </div>
+          )}
         </div>
       )}
     </Fragment>
